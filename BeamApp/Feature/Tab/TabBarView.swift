2d67f1456c73474d22566dbce36e28b30448c02b
kim-jiha95 <kimjiha1112@gmail.com>
2024-09-13 18:05:04 +0900

    fix build error while app name change

---
+++ BeamApp/Feature/Tab/TabBarView.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarView.swift
+//  BeamApp
+//
+//  Created by freed on 9/13/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct TabBarView: View {
+    let store: StoreOf<AppReducer>
+    
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            TabView(selection: viewStore.binding(
+                get: \.selectedTab,
+                send: AppReducer.Action.setSelectedTab
+            )) {
+                HomeView(isLoggedIn: viewStore.binding(
+                    get: \.isLoggedIn,
+                    send: { .setLoggedIn($0) }
+                ))
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+                .tag(AppReducer.Tab.home)
+                
+                PlayerView()
+                .tabItem {
+                    Label("Player", systemImage: "play.circle.fill")
+                    }
+                .tag(AppReducer.Tab.player)
+            }
+        }
+    }
+}
